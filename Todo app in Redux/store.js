8b5861af0cfde5a4120745ac40f03f1ea237de99
Deepika590 <deepikas984319@gmail.com>
2024-12-28 17:40:29 +0530

    Add files via upload

---
+++ Todo app in Redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer  from './Slices/UserSlices';
+
+const store = configureStore({
+    devTools: true,
+    reducer:{
+        Users:userReducer
+    }
+})
+export default store;
